68ded4b41be876a5676b9686ce352ea4b90aa07e
SagarOnline <chopade.sagar@gmail.com>
2019-06-29 14:54:07 +0530

    fixed compression issues

--- webpack.config.js
+++ webpack.config.js
@@ -22,12 +22,15 @@ module.exports = {
     ],*/
     devServer:{
         https: true,
+	port: 8080,
+	host: '0.0.0.0',
         proxy: {
             '/': {
-              target: 'https://chartink.com/',
+              target: 'https://192.168.42.222:8443/',
               secure: false,
               //ignorePath: true,
             }
         }
     }
+
 }
